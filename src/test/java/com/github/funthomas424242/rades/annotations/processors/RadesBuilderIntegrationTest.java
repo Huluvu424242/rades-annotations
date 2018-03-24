1e85c414dda2a931550236f34fa2e13ab07a8037
FunThomas424242 <funthomas424242@gmail.com>
2018-03-24 20:00:08 +0100

    Test umbenannt
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderIntegrationTest.java
+++ src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderIntegrationTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
-public class RadesBuilderProcessorTest {
+public class RadesBuilderIntegrationTest {
 
     @Test
     @DisplayName("Alle Felder gültig befüllen.")
