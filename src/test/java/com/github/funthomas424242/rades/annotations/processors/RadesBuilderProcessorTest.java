15f7091903d9d1ff9b3261b1375dc635a5a788b3
FunThomas424242 <funthomas424242@gmail.com>
2018-03-24 19:54:58 +0100

    Junit 5 applied
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderProcessorTest.java
+++ src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderProcessorTest.java
@@ -3,14 +3,19 @@ package com.github.funthomas424242.rades.annotations.processors;
 import com.github.funthomas424242.domain.Person;
 import com.github.funthomas424242.domain.PersonBuilder;
 import com.github.funthomas424242.rades.annotations.RadesBuilder;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class RadesBuilderProcessorTest {
 
     @Test
+    @DisplayName("Alle Felder gültig befüllen.")
+    @Tags({@Tag("integration"),@Tag("builder")})
     public void testAlleFelderBefuellt(){
         final Person person = new PersonBuilder()
                 .withVorname("Max")
