20d35e74a96d198e71e6b3ef98fba8b7a11fc843
FunThomas424242 <funthomas424242@gmail.com>
2018-03-25 15:42:43 +0200

    AutoService aktiviert
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderProcessor.java
+++ src/main/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderProcessor.java
@@ -1,6 +1,6 @@
 package com.github.funthomas424242.rades.annotations.processors;
 
-//import com.google.auto.service.AutoService;
+import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
@@ -17,7 +17,7 @@ import java.util.Set;
 
 @SupportedAnnotationTypes("com.github.funthomas424242.rades.annotations.RadesBuilder")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
-//@AutoService(Processor.class)
+@AutoService(Processor.class)
 public class RadesBuilderProcessor extends AbstractProcessor {
 
     protected ProcessingEnvironment processingEnvironment;
@@ -40,7 +40,7 @@ public class RadesBuilderProcessor extends AbstractProcessor {
                     continue;
                 }
                 final TypeElement typeElement = (TypeElement) annotatedElement;
-                final Map<Name, Name> mapName2Type = new HashMap<Name, Name>();
+                final Map<Name, Name> mapName2Type = new HashMap<>();
                 final List<? extends Element> classMembers = annotatedElement.getEnclosedElements();
                 for (final Element classMember : classMembers) {
                     if (classMember.getKind().isField()) {
@@ -86,8 +86,7 @@ public class RadesBuilderProcessor extends AbstractProcessor {
         JavaFileObject builderFile = processingEnv.getFiler()
                 .createSourceFile(builderClassName);
 
-        try {
-            final PrintWriter  out = new PrintWriter(builderFile.openWriter());
+        try (PrintWriter out = new PrintWriter(builderFile.openWriter())) {
 
             if (packageName != null) {
                 out.print("package ");
@@ -117,8 +116,7 @@ public class RadesBuilderProcessor extends AbstractProcessor {
             out.println("    }");
             out.println();
 
-            final Set<Map.Entry<Name,Name>> entries= mapFieldName2Type.entrySet();
-            for(final Map.Entry fields : entries){
+            mapFieldName2Type.entrySet().forEach(fields -> {
                 final String fieldName = fields.getKey().toString();
                 final String attributName = fieldName.toString();
                 final String setterName = "with" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
@@ -139,34 +137,9 @@ public class RadesBuilderProcessor extends AbstractProcessor {
                 out.println("        return this;");
                 out.println("    }");
                 out.println();
-            }
-
-//            forEach(fields -> {
-//                final String fieldName = fields.getKey().toString();
-//                final String attributName = fieldName.toString();
-//                final String setterName = "with" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-//                final String argumentType = fields.getValue().toString();
-//
-//                out.print("    public ");
-//                out.print(builderSimpleClassName);
-//                out.print(" ");
-//                out.print(setterName);
-//
-//                out.print("( final ");
-//
-//                out.print(argumentType);
-//                out.println(" " + attributName + " ) {");
-//                out.print("        this." + objectName + ".");
-//                out.print(attributName);
-//                out.println(" = " + attributName + ";");
-//                out.println("        return this;");
-//                out.println("    }");
-//                out.println();
-//            });
+            });
 
             out.println("}");
-        }finally {
-            System.out.println("Finnally");
         }
     }
 
--- src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderIntegrationTest.java
+++ src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.github.funthomas424242.rades.annotations.processors;
 
 import com.github.funthomas424242.domain.Person;
+import com.github.funthomas424242.domain.PersonBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
@@ -15,11 +16,11 @@ public class RadesBuilderIntegrationTest {
     @DisplayName("Alle Felder gültig befüllen.")
     @Tags({@Tag("integration"),@Tag("builder")})
     public void testAlleFelderBefuellt(){
-//        final Person person = new PersonBuilder()
-//                .withVorname("Max")
-//                .withName("Mustermann")
-//                .build();
-//        assertNotNull(person);
+        final Person person = new PersonBuilder()
+                .withVorname("Max")
+                .withName("Mustermann")
+                .build();
+        assertNotNull(person);
     }
 
 }
