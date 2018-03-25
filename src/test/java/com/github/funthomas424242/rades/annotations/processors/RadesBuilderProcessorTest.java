30f90e5292d2b55829308eba0aa51a9b67fb9045
FunThomas424242 <funthomas424242@gmail.com>
2018-03-25 17:25:13 +0200

    Weiteren Unit Testfall realisiert
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderProcessorTest.java
+++ src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderProcessorTest.java
@@ -34,44 +34,44 @@ public class RadesBuilderProcessorTest {
     public void initTest() {
     }
 
-//    @Test
-//    public void processTest() throws MalformedURLException {
-//        final String testFolderRoot = "src/test/java/";
-//        final String pathPersonJava = "com/github/funthomas424242/domain/Person.java";
-//        final URL urlPersonJava = getResourceURL(testFolderRoot, pathPersonJava);
-//
-//        final Compilation compilation = javac()
-//                .withProcessors(new RadesBuilderProcessor())
-//                .compile(JavaFileObjects.forResource(urlPersonJava));
-//
-//        assertThat(compilation).succeeded();
-//        assertThat(compilation)
-//                .generatedFile(StandardLocation.SOURCE_OUTPUT, "com.github.funthomas424242.domain", "PersonBuilder.java")
-//                .hasSourceEquivalentTo(JavaFileObjects.forSourceString("com.github.funthomas424242.domain.PersonBuilder", "package com.github.funthomas424242.domain;\n" +
-//                        "\n" +
-//                        "public class PersonBuilder {\n" +
-//                        "\n" +
-//                        "    private Person person = new Person();\n" +
-//                        "\n" +
-//                        "    public Person build() {\n" +
-//                        "        final Person value = this.person;\n" +
-//                        "        this.person = null;\n" +
-//                        "        return value;\n" +
-//                        "    }\n" +
-//                        "\n" +
-//                        "    public PersonBuilder withVorname( final String vorname ) {\n" +
-//                        "        this.person.vorname = vorname;\n" +
-//                        "        return this;\n" +
-//                        "    }\n" +
-//                        "\n" +
-//                        "    public PersonBuilder withName( final String name ) {\n" +
-//                        "        this.person.name = name;\n" +
-//                        "        return this;\n" +
-//                        "    }\n" +
-//                        "\n" +
-//                        "}\n")
-//                );
-//    }
+    @Test
+    public void processTest() throws MalformedURLException {
+        final String testFolderRoot = "src/test/java/";
+        final String pathPersonJava = "com/github/funthomas424242/domain/Person.java";
+        final URL urlPersonJava = getResourceURL(testFolderRoot, pathPersonJava);
+
+        final Compilation compilation = javac()
+                .withProcessors(new RadesBuilderProcessor())
+                .compile(JavaFileObjects.forResource(urlPersonJava));
+
+        assertThat(compilation).succeeded();
+        assertThat(compilation)
+                .generatedFile(StandardLocation.SOURCE_OUTPUT, "com.github.funthomas424242.domain", "PersonBuilder.java")
+                .hasSourceEquivalentTo(JavaFileObjects.forSourceString("com.github.funthomas424242.domain.PersonBuilder", "package com.github.funthomas424242.domain;\n" +
+                        "\n" +
+                        "public class PersonBuilder {\n" +
+                        "\n" +
+                        "    private Person person = new Person();\n" +
+                        "\n" +
+                        "    public Person build() {\n" +
+                        "        final Person value = this.person;\n" +
+                        "        this.person = null;\n" +
+                        "        return value;\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    public PersonBuilder withVorname( final String vorname ) {\n" +
+                        "        this.person.vorname = vorname;\n" +
+                        "        return this;\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    public PersonBuilder withName( final String name ) {\n" +
+                        "        this.person.name = name;\n" +
+                        "        return this;\n" +
+                        "    }\n" +
+                        "\n" +
+                        "}\n")
+                );
+    }
 
     @Test
     public void shouldCompileClassWithoutIgnoreAnnotationWithoutErrors() throws MalformedURLException {
