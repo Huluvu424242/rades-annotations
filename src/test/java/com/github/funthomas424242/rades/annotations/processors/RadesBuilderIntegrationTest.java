e45f3f2bac929c21a3044cb88bafd69de8b8ee1e
FunThomas424242 <funthomas424242@gmail.com>
2018-03-26 21:48:16 +0200

    Tests für Abteilung und Person angepasst
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderIntegrationTest.java
+++ src/test/java/com/github/funthomas424242/rades/annotations/processors/RadesBuilderIntegrationTest.java
@@ -2,6 +2,8 @@ package com.github.funthomas424242.rades.annotations.processors;
 
 import com.github.funthomas424242.domain.Abteilung;
 import com.github.funthomas424242.domain.AbteilungBuilder;
+import com.github.funthomas424242.domain.Person;
+import com.github.funthomas424242.domain.PersonBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
@@ -13,14 +15,25 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class RadesBuilderIntegrationTest {
 
     @Test
-    @DisplayName("Alle Felder gültig befüllen.")
+    @DisplayName("Alle Felder von Abteilung gültig befüllen.")
     @Tags({@Tag("integration"), @Tag("builder")})
-    public void testAlleFelderBefuellt() {
+    public void testAbteilungAlleFelderBefuellt() {
         final Abteilung abteilung = new AbteilungBuilder()
                 .withName("Musterabteilung")
                 .withAbteilungsNr("IT-8788")
                 .build();
         assertNotNull(abteilung);
+    }
+
+    @Test
+    @DisplayName("Alle Felder von Person gültig befüllen.")
+    @Tags({@Tag("integration"), @Tag("builder")})
+    public void testPersonAlleFelderBefuellt() {
+        final Person person = new PersonBuilder()
+                .withName("Mustermann")
+                .withVorname("Max")
+                .build();
+        assertNotNull(person);
     }
 
 }
