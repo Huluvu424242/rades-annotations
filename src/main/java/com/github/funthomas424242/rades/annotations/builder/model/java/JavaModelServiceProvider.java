2ada24e6bade4d34e70f1f55e8e7a43ce352ad5a
FunThomas424242 <funthomas424242@gmail.com>
2018-04-14 13:22:30 +0200

    isoDate Methode ausgelagert
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rades/annotations/builder/model/java/JavaModelServiceProvider.java
+++ src/main/java/com/github/funthomas424242/rades/annotations/builder/model/java/JavaModelServiceProvider.java
@@ -1,25 +1,21 @@
 package com.github.funthomas424242.rades.annotations.builder.model.java;
 
-import com.github.funthomas424242.rades.annotations.builder.model.java.JavaModelService;
-import com.github.funthomas424242.rades.annotations.builder.model.java.JavaSrcFileCreator;
+import com.github.funthomas424242.rades.annotations.lang.java.JavaModelHelper;
 import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.Filer;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @AutoService(JavaModelService.class)
 public class JavaModelServiceProvider implements JavaModelService {
 
     @Override
     public JavaSrcFileCreator getJavaSrcFileCreator(final Filer filer, final String className) {
-        return new JavaSrcFileCreator(filer, className,this);
+        return new JavaSrcFileCreator(filer, className, this);
     }
 
     @Override
     public String getNowAsISOString() {
-        final LocalDateTime now = LocalDateTime.now();
-        return now.format(DateTimeFormatter.ISO_DATE_TIME);
+        return JavaModelHelper.getNowAsISOString();
     }
 
 }
--- src/main/java/com/github/funthomas424242/rades/annotations/lang/java/JavaModelHelper.java
+++ src/main/java/com/github/funthomas424242/rades/annotations/lang/java/JavaModelHelper.java
@@ -1,13 +1,21 @@
 package com.github.funthomas424242.rades.annotations.lang.java;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class JavaModelHelper {
 
-    public static String computePackageName(final String fullQualifiedClassName){
-        String packageName=null;
+    public static String computePackageName(final String fullQualifiedClassName) {
+        String packageName = null;
         int lastDot = fullQualifiedClassName.lastIndexOf('.');
         if (lastDot > 0) {
             packageName = fullQualifiedClassName.substring(0, lastDot);
         }
         return packageName;
+    }
+
+    public static String getNowAsISOString() {
+        final LocalDateTime now = LocalDateTime.now();
+        return now.format(DateTimeFormatter.ISO_DATE_TIME);
     }
 }
