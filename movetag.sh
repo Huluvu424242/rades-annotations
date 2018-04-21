b67169ddc6219a338738d4252df146a338b3923d
FunThomas424242 <funthomas424242@gmail.com>
2018-04-21 16:26:35 +0200

    move tag script added
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ movetag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+    echo Bitte Tagnamen als Parameter angeben
+else
+    git fetch
+    git pull
+    git push origin :refs/tags/$1
+    git tag -f $1
+    git push origin master --tags
+fi
