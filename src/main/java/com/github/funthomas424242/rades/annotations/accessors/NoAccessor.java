3a44cf19ae944c9855bffb6a4e9810b43bcbafee
FunThomas424242 <funthomas424242@gmail.com>
2018-04-22 18:58:53 +0200

    #62 und #60 vorbereitet
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ src/main/java/com/github/funthomas424242/rades/annotations/accessors/NoAccessor.java
@@ -0,0 +1,21 @@
+package com.github.funthomas424242.rades.annotations.accessors;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Verhindert die Generierung eines Getter
+ *
+ * @version $Version$, $Date$
+ * @since 2.2.0
+ */
+@Documented
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.SOURCE)
+public @interface NoAccessor {
+
+}
---
+++ src/main/java/com/github/funthomas424242/rades/annotations/builder/NoBuilder.java
@@ -0,0 +1,21 @@
+package com.github.funthomas424242.rades.annotations.builder;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Verhindert gie Generierung einer with Methode am Builder.
+ *
+ * @version $Version$, $Date$
+ * @since 2.2.0
+ */
+@Documented
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.SOURCE)
+public @interface NoBuilder {
+
+}
