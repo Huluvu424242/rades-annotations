b063150859a71c726258b8fc34636448c62effde
FunThomas424242 <funthomas424242@gmail.com>
2018-04-30 22:29:01 +0200

    Codacy Issues fixed
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/rades/annotations/accessors/RadesAccessorCompatibillityTest.java
+++ src/test/java/com/github/funthomas424242/rades/annotations/accessors/RadesAccessorCompatibillityTest.java
@@ -20,35 +20,35 @@ public class RadesAccessorCompatibillityTest {
     @Test
     @DisplayName("Prüfe korrektes Package für @AddAccessor")
     @Tags({@Tag("compatibillity"), @Tag("accessor")})
-    void testAddAccesoorPackageName() {
+    public void testAddAccesoorPackageName() {
         assertEquals("com.github.funthomas424242.rades.annotations.accessors", AddAccessor.class.getPackage().getName());
     }
 
     @Test
     @DisplayName("Prüfe korrektes Package für @RadesAddAccessor")
     @Tags({@Tag("compatibillity"), @Tag("accessor")})
-    void testRadesAddAccessorPackageName() {
+    public void testRadesAddAccessorPackageName() {
         assertEquals("com.github.funthomas424242.rades.annotations.accessors", RadesAddAccessor.class.getPackage().getName());
     }
 
     @Test
     @DisplayName("Prüfe korrektes Package für @RadesNoAccessor")
     @Tags({@Tag("compatibillity"), @Tag("accessor")})
-    void testRadesNoAccessorPackageName() {
+    public void testRadesNoAccessorPackageName() {
         assertEquals("com.github.funthomas424242.rades.annotations.accessors", RadesNoAccessor.class.getPackage().getName());
     }
 
     @Test
     @DisplayName("Prüfe korrektes Package für @NoAccessor")
     @Tags({@Tag("compatibillity"), @Tag("accessor")})
-    void testNoAccessorPackageName() {
+    public void testNoAccessorPackageName() {
         assertEquals("com.github.funthomas424242.rades.annotations.accessors", NoAccessor.class.getPackage().getName());
     }
 
     @Test
     @DisplayName("Prüfe korrektes Delegation für toString() Methode im Accessor von Person.")
     @Tags({@Tag("compatibillity"), @Tag("accessor")})
-    void testToStringValidDelegation() {
+    public void testToStringValidDelegation() {
         final Person person = new PersonBuilder().withName("Kratzert").withVorname("Johanna").build();
         final PersonAccessor personAccessor = new PersonAccessor(person);
         assertEquals("Kratzert, Johanna", personAccessor.toString());
@@ -57,7 +57,7 @@ public class RadesAccessorCompatibillityTest {
     @Test
     @DisplayName("Prüfe korrekte fehlende Delegation für toString() Methode im Accessor von Tier.")
     @Tags({@Tag("compatibillity"), @Tag("accessor")})
-    void testToStringInvalideDelegation() {
+    public void testToStringInvalideDelegation() {
         final Tier tier = new TierBuilder().build();
         final TierAccessor tierAccessor = new TierAccessor(tier);
         assertNotEquals(tier.toString(), tierAccessor.toString());
