89404c922d469565038b59cc9167f27d214b7967
FunThomas424242 <funthomas424242@gmail.com>
2018-04-30 22:29:30 +0200

    Codacy Issues fixed
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/rades/annotations/builder/RadesBuilderCompatibillityTest.java
+++ src/test/java/com/github/funthomas424242/rades/annotations/builder/RadesBuilderCompatibillityTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class RadesBuilderCompatibillityTest {
 
